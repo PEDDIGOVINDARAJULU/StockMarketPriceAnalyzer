c7066340211acaa68557ef744a64c02458cbf5c5
peddi govinda rajulu <snowflaketrailmay16@gmail.com>
2026-06-11 18:10:46 +0000

    checking dbt state awareness by adding freshness configuration

--- models/dbt_state_awareness/target_model.sql
+++ models/dbt_state_awareness/target_model.sql
@@ -1,2 +1,8 @@
-{{config(materialized='table')}}
+{{config(materialized='table',freshness={
+        "build_after": {
+        "count": 2,
+        "period": "hour",
+        "updates_on": "any"
+        } 
+      })}}
 SELECT * FROM {{ref('source_model')}}
